56b70792a41cd8722cf83be76508c856f8ad3ce8
Ashutosh <ashu9326@gmail.com>
2024-03-09 12:29:39 +0530

    no changes

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -18,11 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from home.views import *
 
+
 urlpatterns = [
-    path("", home ,name="home"),
-    path("contact/", contact ,name="contact"),
-    path("about/", about ,name="about"),
-
+    path('', home, name='home'),
+    path('about/', about,name='about'),
+    path('contact/', contact,name='contact'),
     path('admin/', admin.site.urls),
-
 ]
+
